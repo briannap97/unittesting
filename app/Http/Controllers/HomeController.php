4cf392c675da7dc0ece6ec9bf59e9ffefbab3315
briannap <bp275@njit.edu>
2018-10-19 18:43:32 -0400

    Feature: printed $user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -26,8 +27,10 @@ class HomeController extends Controller
         return view('home');
     }
 
-    public function about()
+    public function profile()
     {
-        return view('about', ['myvariable' => 'Hello World']);
+        $user = Auth::user();
+        dd($user);
+        return view('profile', ['myvariable' => 'Hello World']);
     }
 }
